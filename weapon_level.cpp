b8fafcef5c07b20534ca610e77b7027d6a78ce1c
blissard <blissardartinq@gmail.com>
2021-02-02 18:41:06 +0530

    Added level basedvalues for Prototype Crescent

---
+++ weapon_level.cpp
@@ -0,0 +1,34 @@
+int prototype_atkB[]{
+    42, // Level 1
+    56, 74, 91, 109,
+    135, // Level 20 / 40 Ascension 1
+    152, 170, 187, 205,
+    231, // Level 40 / 50 Ascension 2
+    248, 266,
+    292, // Level 50 / 60 Ascension 3
+    309, 327, 353, 370, 388,
+    414, // Level 60 / 70 Ascension 4
+    431, 449,
+    475, // Level 80 / 90 Ascenions 5
+    492,
+    510 // Max level
+};
+
+float prototype_passive[]{
+    9,    // Level 1
+    10.5, 12.3, 14.1, 15.9,
+    15.9, // Level 20 / 40 
+    17.7, 19.5, 21.4, 23.2,
+    23.2, // Level 40 / 50 
+    25, 26.8, 26.8,
+    28.6, // Level 50 / 60 
+    30.4,
+    30.4, // Level 60 / 70 
+    32.3, 34.1,
+    34.1, // level 70 / 80 
+    35.9, 37.7,
+    37.7, // Level 80 / 90 
+    39.5,
+    41.3 // Max level 90
+
+};
