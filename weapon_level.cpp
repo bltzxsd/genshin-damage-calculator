8d8e577a97c7dc4e3c8003299d4b4996ec21d4e6
blissard <blissardartinq@gmail.com>
2021-02-02 19:23:01 +0530

    weapon_level.cpp amos base atk and substat added

--- weapon_level.cpp
+++ weapon_level.cpp
@@ -6,15 +6,17 @@ int prototype_atkB[]{
     231, // Level 40 / 50 Ascension 2
     248, 266,
     292, // Level 50 / 60 Ascension 3
-    309, 327, 353, 370, 388,
-    414, // Level 60 / 70 Ascension 4
+    309, 327, 
+    353, // Level 60 / 70 Asecnsion 4 
+    370, 388,
+    414, // Level 70 / 80 Ascension 5
     431, 449,
-    475, // Level 80 / 90 Ascenions 5
+    475, // Level 80 / 90 Ascenions 6
     492,
     510 // Max level
 };
 
-float prototype_passive[]{
+float prototype_substat[]{
     9,    // Level 1
     10.5, 12.3, 14.1, 15.9,
     15.9, // Level 20 / 40 
@@ -30,5 +32,40 @@ float prototype_passive[]{
     37.7, // Level 80 / 90 
     39.5,
     41.3 // Max level 90 
+};
 
+int amos_atkB[]{
+    46, // Level 1
+    62, 82, 102, 122,
+    153, // Level 20 / 40 Ascension 1
+    173, 194, 214, 235,
+    266, // Level 40 / 50 Ascension 2 
+    287, 308,
+    340, // Level 50 / 60 Ascension 3
+    361, 382,
+    414, // Level 60 / 70 Ascension 4
+    435, 457,
+    488, // Level 70 / 80 Ascension 5
+    510, 532,
+    563, // Level 80 / 90 Ascension 6
+    586,
+    608 // Max level 90
+};
+
+float amos_substat[]{
+    10.8, // Level 1
+    12.5, 14.7, 16.9, 19.1,
+    19.1, // Level 20 / 40 
+    21.3, 23.4, 25.6, 27.8,
+    27.8, // Level 40 / 50 
+    30, 32.2,
+    32.2, // Level 50 / 60
+    34.4, 36.5,
+    36.5, // Level 60 / 70
+    38.7,40.9,
+    40.9, // Level 70 / 80
+    43.1, 45.3,
+    45.3, // Level 80 / 90
+    47.7,
+    49.6 // Max Level 90
 };
