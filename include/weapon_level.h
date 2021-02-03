4478aa1821ca4156ba68f162ce9086b583fc200f
blissard <blissardartinq@gmail.com>
2021-02-03 18:46:01 +0530

    formatted correctly

--- include/weapon_level.h
+++ include/weapon_level.h
@@ -1,9 +1,10 @@
 #ifndef WEAPON_LEVEL
 #define WEAPON_LEVEL
-    #define PROTOTYPE 99
-    #define AMOS 98
-    extern int prototype_atkB[];
-    extern float prototype_substat[];
-    extern int amos_atkB[];
-    extern float amos_substat[];
+#define PROTOTYPE 99
+#define AMOS 98
+
+extern int prototype_atkB[];
+extern float prototype_substat[];
+extern int amos_atkB[];
+extern float amos_substat[];
 #endif  
--- include/weaponlist.h
+++ include/weaponlist.h
@@ -1,12 +1,12 @@
 #ifndef WEAPON_LIST
 #define WEAPON_LIST
-    #include <string>
-    #include <iostream>
-    #include <array>
+#include <string>
+#include <iostream>
+#include <array>
 
-    extern std::string bows[];
-    int weapon_name();
-    int level_chk(int);
-    float substat_chk(int);
-    float passive_chk(float);
+extern std::string bows[];
+int weapon_name();
+int level_chk(int);
+float substat_chk(int);
+float passive_chk(float);
 #endif
