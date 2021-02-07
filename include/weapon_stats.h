df30b2da2ab0152b21d4cccd45cf59bc11a29507
blissard <blissardartinq@gmail.com>
2021-02-07 16:08:00 +0530

    added defines for the corresponding cpp file

--- include/weapon_stats.h
+++ include/weapon_stats.h
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <array>
 
+#define AMOS_PASSIVE_BOOST 52
+#define CRESCENT_PASSIVE_BOOST 36
+
 extern std::string bows[];
 int weapon_name();
 std::string level_chk(int);
