8c9486b8c9994c7e33cf18568b9d80fd93008231
blissard <blissardartinq@gmail.com>
2021-02-08 23:40:59 +0530

    updated outputs

--- artifact_set.cpp
+++ artifact_set.cpp
@@ -16,22 +16,23 @@ std::string artifact_set_name[] {
 };
 
 int artifact_set_chk() {
-    std::cout << "Enter the artifact set you are using: ";
+    std::cout << "Enter the artifact set you are using (Wanderer's Troupe / Blizzard Strayer): ";
     std::string artifact_used{string_input()};
     for (int x {0}; x <= (int) std::size(artifact_set_name); ++x) {
         if (artifact_used == artifact_set_name[x]) {
             if ((artifact_used[0] == 'W') || (artifact_used[0] == 'w')) {
-                std::cout << "You're using Wanderer's Troupe set. 4PC (optimal)\n";
+                std::cout << "You're using Wanderer's Troupe set. 4PC (optimal)\n\n";
                 int wanderer_boost{35};
                 return wanderer_boost;
             }
             if ((artifact_used[0] == 'B') || (artifact_used[0] == 'b')) {
-                std::cout << "You're using Blizzard Strayer set. 4PC (optimal)\n";
+                std::cout << "You're using Blizzard Strayer set. 4PC (optimal)\n\n";
                 int blizzard_boost{15};
                 return blizzard_boost;
             }
         }
     }
-    std::cout << "Not a valid arfifact\ni.e: NOT Wanderer's Troupe / Blizzard Strayer\n";
+    std::cout << "Not a valid arfifact\ni.e: NOT Wanderer's Troupe / Blizzard Strayer\n"
+              << "Warning: Artifact values will not be included in this calculation.\n";
     return 1;
 }
