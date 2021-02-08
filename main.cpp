18cd074897ace3cee07722f81bd2985413fda0fe
blissard <blissardartinq@gmail.com>
2021-02-08 20:51:47 +0530

    general update

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include "include/weapon_stats.h"
 #include "include/weapon_level.h"
 #include "include/artifact_value.h"
+#include "include/artifact_set.h"
 
-int main()
-{
+int main() {
 
     // Get level
     std::cout << "Enter your Ganyu's level: ";
@@ -22,8 +22,25 @@ int main()
     passive_chk(weaponName);
     ignore_line();
 
+    // Get which set is being used
+    artifact_set_chk();
+    ignore_line();
     // Get Flat ATK from artifact
     flat_atk_get();
     // Get ATK% from artifact
     atk_perc_get();
+
+    // calculate the sum of all flat atk given
+    int total_flat_atk{0};
+    for (auto& x : flat_atk) {
+        total_flat_atk += x;
+    }
+    // calculate the sum of all atk% given
+    int total_perc_atk{0};
+    for (auto& x : atk_percent) {
+        total_perc_atk += x;
+    }
+
+    std::cout << "Total flat ATK: " << total_flat_atk << '\n'
+              << "Total ATK%: " << total_perc_atk << '%' << '\n';
 }
