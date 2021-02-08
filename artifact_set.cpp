1d4ad2b22b20d1d55a4b62352c8115abc987961a
blissard <blissardartinq@gmail.com>
2021-02-08 17:54:43 +0530

    added artifact checking prototype

---
+++ artifact_set.cpp
@@ -0,0 +1,38 @@
+#include "include/artifact_set.h"
+
+std::string artifact_set_name[] {
+    "Blizzard Strayer",
+    "Wanderer's Troupe",
+    "blizzard",
+    "Blizzard",
+    "strayer",
+    "Strayer"
+    "Wanderer",
+    "wanderer's",
+    "Wanderer's",
+    "troupe",
+    "Troupe",
+    "wanderer"
+};
+
+int artifact_set_chk() {
+    std::cout << "Enter the artifact set you are using: ";
+    std::string artifact_used{string_input()};
+    for (int x; x >= std::size(artifact_set_name); ++x) {
+        if (artifact_used != artifact_set_name[x]) {
+            std::cout << "The artifact given is not a valid artifact\n";
+            std::cout << "i.e: not Wanderer's Troupe / Blizzard Strayer\n";
+            return 1;
+        } 
+        if ((artifact_used[0] == 'W') || (artifact_used[0] == 'w')) {
+            std::cout << "You're using Wanderer's Troupe set. 4PC (optimal)\n";
+            int wanderer_boost{35};
+            return wanderer_boost;
+        } else if ((artifact_used[0] == 'B') || (artifact_used[0] == 'b')) {
+            std::cout << "You're using Blizzard Strayer set. 4PC (optimal)\n";
+            int blizzard_boost{15};
+            return blizzard_boost;
+        }
+    }
+    return 1;
+}
---
+++ include/artifact_set.h
@@ -0,0 +1,13 @@
+#ifndef ARTIFACT_SET
+#define ARTIFACT_SET 
+
+#define WANDERERS 21
+#define BLIZZARD 22
+
+#include "inputhandler.h"
+#include <string>
+
+extern std::string artifact_set_name[];
+int artifact_set_chk();
+
+#endif
