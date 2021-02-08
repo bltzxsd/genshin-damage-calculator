937b504ba2233b5f5603e990b94a114881e8e38f
blissard <blissardartinq@gmail.com>
2021-02-08 20:48:35 +0530

    Corrected some stuff

--- artifact_set.cpp
+++ artifact_set.cpp
@@ -6,33 +6,32 @@ std::string artifact_set_name[] {
     "blizzard",
     "Blizzard",
     "strayer",
-    "Strayer"
+    "Strayer",
     "Wanderer",
-    "wanderer's",
+    "wanderers",
     "Wanderer's",
     "troupe",
     "Troupe",
-    "wanderer"
+    "wanderer",
 };
 
 int artifact_set_chk() {
     std::cout << "Enter the artifact set you are using: ";
     std::string artifact_used{string_input()};
-    for (int x = 0; x >= std::size(artifact_set_name); ++x) {
-        if (artifact_used != artifact_set_name[x]) {
-            std::cout << "The artifact given is not a valid artifact\n";
-            std::cout << "i.e: not Wanderer's Troupe / Blizzard Strayer\n";
-            return 1;
-        } 
-        if ((artifact_used[0] == 'W') || (artifact_used[0] == 'w')) {
-            std::cout << "You're using Wanderer's Troupe set. 4PC (optimal)\n";
-            int wanderer_boost{35};
-            return wanderer_boost;
-        } else if ((artifact_used[0] == 'B') || (artifact_used[0] == 'b')) {
-            std::cout << "You're using Blizzard Strayer set. 4PC (optimal)\n";
-            int blizzard_boost{15};
-            return blizzard_boost;
+    for (int x {0}; x <= (int) std::size(artifact_set_name); ++x) {
+        if (artifact_used == artifact_set_name[x]) {
+            if ((artifact_used[0] == 'W') || (artifact_used[0] == 'w')) {
+                std::cout << "You're using Wanderer's Troupe set. 4PC (optimal)\n";
+                int wanderer_boost{35};
+                return wanderer_boost;
+            }
+            if ((artifact_used[0] == 'B') || (artifact_used[0] == 'b')) {
+                std::cout << "You're using Blizzard Strayer set. 4PC (optimal)\n";
+                int blizzard_boost{15};
+                return blizzard_boost;
+            }
         }
     }
+    std::cout << "Not a valid arfifact\ni.e: NOT Wanderer's Troupe / Blizzard Strayer\n";
     return 1;
 }
