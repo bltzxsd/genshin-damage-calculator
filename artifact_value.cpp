1f7f384c55d10f43e587c2d89a80e54adf55e767
blissard <blissardartinq@gmail.com>
2021-02-09 15:43:37 +0530

    now explicitly calls template type

--- artifact_value.cpp
+++ artifact_value.cpp
@@ -8,35 +8,35 @@ void flat_atk_get() {
         switch (x) {
             case 1: {
                 std::cout << "Enter the total FLAT ATK on your Circlet of Logos (substat): ";
-                float circlet{input_handler_range(0, MAX_FLAT_ATK)};
+                int circlet{input_handler_range<int>(0, MAX_FLAT_ATK)};
                 flat_atk[0] = circlet;
                 ignore_line();
             break;
             }
             case 2: {
                 std::cout << "Enter the total FLAT ATK on your Goblet of Eonothem (substat): ";
-                float goblet{input_handler_range(0, MAX_FLAT_ATK)};
+                int goblet{input_handler_range<int>(0, MAX_FLAT_ATK)};
                 flat_atk[1] = goblet;
                 ignore_line();
             break;
             }
             case 3: {
                 std::cout << "Enter the total FLAT ATK on your Sands of Eon (substat): ";
-                float cup{input_handler_range(0, MAX_FLAT_ATK)};
+                int cup{input_handler_range<int>(0, MAX_FLAT_ATK)};
                 flat_atk[2] = cup;
                 ignore_line();
             break;
             }
             case 4: {
                 std::cout << "Enter the total FLAT ATK on your Flower of Life (substat): ";
-                float flower{input_handler_range(0, MAX_FLAT_ATK)};
+                int flower{input_handler_range<int>(0, MAX_FLAT_ATK)};
                 flat_atk[3] = flower;
                 ignore_line();
             break;
             }
             case 5: {
                 std::cout << "Enter the FLAT ATK on your Plume Of Death (mainstat): ";
-                float feather{input_handler_range(0, FEATHER_FLAT_MAX_ATK)};
+                int feather{input_handler_range<int>(0, FEATHER_FLAT_MAX_ATK)};
                 flat_atk[4] = feather;
                 ignore_line();
             break;
