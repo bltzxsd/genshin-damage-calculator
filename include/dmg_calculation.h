5068228210803c56d9c91e7c94e65a74a13aad3d
blissard <blissardartinq@gmail.com>
2021-02-16 00:40:41 +0530

    wrapped functions in namespace

--- include/dmg_calculation.h
+++ include/dmg_calculation.h
@@ -3,9 +3,12 @@
 
 #include "inputhandler.h"
 
-int crit_dmg(int, float);
-int em_perc_bonus(int);
-int basic_atk(int, float, int);
-int outgoing_dmg(int, int, int);
-int amping_dmg(int, float, int);
+namespace dmgCalc {
+    int crit_dmg(int, float);
+    int em_perc_bonus(int);
+    int basic_atk(int, float, int);
+    int outgoing_dmg(int, int, int);
+    int amping_dmg(int, float, int);
+};
+
 #endif
