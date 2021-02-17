dcdff290e094f29498abfe1acabaea2d834837c3
b1issard <blissardartinq@gmail.com>
2021-02-18 01:24:50 +0530

    removed comment

--- attack_output.cpp
+++ attack_output.cpp
@@ -1,31 +1,16 @@
 #include "include/attack_output.h"
 
-/*
-// checks if amos passive dmg bonus is applied
-    if (dmgbonus_p == AMOS_PASSIVE_BOOST) {
-        dmgbonus_p = AMOS_PASSIVE_BOOST;
-    } else if (dmgbonus_p == CRESCENT_PASSIVE_BOOST) {
-        // if crescent: atk bonus up 34
-        total_perc_atk += CRESCENT_PASSIVE_BOOST;
-    }
-    // check (elemental) dmg bonus
-    if (artifact_set_used == 35 wanderer boost) {
-        dmgbonus_p += 35;
-    } else if (artifact_set_used == 15) {
-        dmgbonus_p += 15;
-    }
-*/
-int auto_talent_lvl;
-float first_atk_ratio{auto_first_atk(auto_talent_lvl)};
-float second_atk_ratio{auto_second_atk(auto_talent_lvl)};
-float third_atk_ratio{auto_third_atk(auto_talent_lvl)};
-float fourth_atk_ratio{auto_fourth_atk(auto_talent_lvl)};
-float fifth_atk_ratio{auto_fifth_atk(auto_talent_lvl)};
-float sixth_atk_ratio{auto_sixth_atk(auto_talent_lvl)};
-float aim_atk_ratio{auto_aimshot(auto_talent_lvl)};
-float charge_atk_ratio{auto_chargeshot(auto_talent_lvl)};
-float frostflake_arrow{auto_frostflake(auto_talent_lvl)};
-float frostflake_bloom{auto_frostflake_bloom(auto_talent_lvl)};
+int talent_level = auto_talent_lvl;
+float first_atk_ratio {auto_first_atk(talent_level)};
+float second_atk_ratio{auto_second_atk(talent_level)};
+float third_atk_ratio {auto_third_atk(talent_level)};
+float fourth_atk_ratio{auto_fourth_atk(talent_level)};
+float fifth_atk_ratio {auto_fifth_atk(talent_level)};
+float sixth_atk_ratio {auto_sixth_atk(talent_level)};
+float aim_atk_ratio   {auto_aimshot(talent_level)};
+float charge_atk_ratio{auto_chargeshot(talent_level)};
+float frostflake_arrow{auto_frostflake(talent_level)};
+float frostflake_bloom{auto_frostflake_bloom(talent_level)};
 
 int weapon_dmg_bonus(float& total_perc_atk, float& dmgbonus_p, float& weapon_passive, float&weapon_name) {
     if (weapon_name == prototype) {
