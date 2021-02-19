bf76bb85a22cf82097435d495b7307f2fc930107
b1issard <blissardartinq@gmail.com>
2021-02-19 12:45:11 +0530

    Removed get to prevent segfault

--- attack_output.cpp
+++ attack_output.cpp
@@ -1,18 +1,17 @@
 #include "include/attack_output.h"
 
-int talent_level = auto_talent_lvl;
-float first_atk_ratio {auto_first_atk(talent_level)};
-float second_atk_ratio{auto_second_atk(talent_level)};
-float third_atk_ratio {auto_third_atk(talent_level)};
-float fourth_atk_ratio{auto_fourth_atk(talent_level)};
-float fifth_atk_ratio {auto_fifth_atk(talent_level)};
-float sixth_atk_ratio {auto_sixth_atk(talent_level)};
-float aim_atk_ratio   {auto_aimshot(talent_level)};
-float charge_atk_ratio{auto_chargeshot(talent_level)};
-float frostflake_arrow{auto_frostflake(talent_level)};
-float frostflake_bloom{auto_frostflake_bloom(talent_level)};
+    float first_atk_ratio{auto_first_atk(auto_talent_level)};
+    float second_atk_ratio{auto_second_atk(auto_talent_level)};
+    float third_atk_ratio{auto_third_atk(auto_talent_level)};
+    float fourth_atk_ratio{auto_fourth_atk(auto_talent_level)};
+    float fifth_atk_ratio{auto_fifth_atk(auto_talent_level)};
+    float sixth_atk_ratio{auto_sixth_atk(auto_talent_level)};
+    float aim_atk_ratio{auto_aimshot(auto_talent_level)};
+    float charge_atk_ratio{auto_chargeshot(auto_talent_level)};
+    float frostflake_arrow{auto_frostflake(auto_talent_level)};
+    float frostflake_bloom{auto_frostflake_bloom(auto_talent_level)};
 
-int weapon_dmg_bonus(float& total_perc_atk, float& dmgbonus_p, float& weapon_passive, float&weapon_name) {
+int weapon_dmg_bonus(float total_perc_atk, float dmgbonus_p, float weapon_passive, float weapon_name) {
     if (weapon_name == prototype) {
         total_perc_atk += weapon_passive;
         return total_perc_atk;
@@ -25,9 +24,9 @@ int weapon_dmg_bonus(float& total_perc_atk, float& dmgbonus_p, float& weapon_pas
     return -1;
 }
 
-float artifact_dmg_bonus(int& artifact_set_used, float& dmgbonus_p, float& dmgbonus_pe) {
+float artifact_dmg_bonus(int artifact_set_used, float dmgbonus_p, float dmgbonus_pe) {
     if (artifact_set_used == blizzard) {
-        return dmgbonus_pe += 15.0f;
+        return dmgbonus_pe += 15.0F;
     }
     if (artifact_set_used == wanderers) {
         return dmgbonus_p += 35;
