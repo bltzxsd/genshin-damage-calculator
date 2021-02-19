f6128a7b09e01cd0322f0f86bd1ca6fef1447ea2
b1issard <blissardartinq@gmail.com>
2021-02-19 23:19:38 +0530

    formatting

--- attack_output.cpp
+++ attack_output.cpp
@@ -1,15 +1,17 @@
 #include "include/attack_output.h"
 
-    float first_atk_ratio{auto_first_atk(auto_talent_level)};
-    float second_atk_ratio{auto_second_atk(auto_talent_level)};
-    float third_atk_ratio{auto_third_atk(auto_talent_level)};
-    float fourth_atk_ratio{auto_fourth_atk(auto_talent_level)};
-    float fifth_atk_ratio{auto_fifth_atk(auto_talent_level)};
-    float sixth_atk_ratio{auto_sixth_atk(auto_talent_level)};
-    float aim_atk_ratio{auto_aimshot(auto_talent_level)};
-    float charge_atk_ratio{auto_chargeshot(auto_talent_level)};
-    float frostflake_arrow{auto_frostflake(auto_talent_level)};
-    float frostflake_bloom{auto_frostflake_bloom(auto_talent_level)};
+float first_atk_ratio{auto_first_atk(auto_talent_level)};
+float second_atk_ratio{auto_second_atk(auto_talent_level)};
+float third_atk_ratio{auto_third_atk(auto_talent_level)};
+float fourth_atk_ratio{auto_fourth_atk(auto_talent_level)};
+float fifth_atk_ratio{auto_fifth_atk(auto_talent_level)};
+float sixth_atk_ratio{auto_sixth_atk(auto_talent_level)};
+float aim_atk_ratio{auto_aimshot(auto_talent_level)};
+float charge_atk_ratio{auto_chargeshot(auto_talent_level)};
+float frostflake_arrow{auto_frostflake(auto_talent_level)};
+float frostflake_bloom{auto_frostflake_bloom(auto_talent_level)};
+
+
 
 int weapon_dmg_bonus(float total_perc_atk, float dmgbonus_p, float weapon_passive, float weapon_name) {
     if (weapon_name == prototype) {
