3ec240c7cb57c51ce12f8d8fd7f0ea7162b53ff6
b1issard <blissardartinq@gmail.com>
2021-02-20 17:54:39 +0530

    fixed datatypes of multipliers

--- attack_output.cpp
+++ attack_output.cpp
@@ -11,8 +11,6 @@ float charge_atk_ratio{auto_chargeshot(auto_talent_level)};
 float frostflake_arrow{auto_frostflake(auto_talent_level)};
 float frostflake_bloom{auto_frostflake_bloom(auto_talent_level)};
 
-
-
 int weapon_dmg_bonus(float total_perc_atk, float dmgbonus_p, float weapon_passive, float weapon_name) {
     if (weapon_name == prototype) {
         total_perc_atk += weapon_passive;
@@ -22,17 +20,18 @@ int weapon_dmg_bonus(float total_perc_atk, float dmgbonus_p, float weapon_passiv
         dmgbonus_p = weapon_passive;
         return dmgbonus_p;
     }
-    std::cerr << "Wrong parameter values: weapon_dmg_bonus(float&, float&, float&, float&)\n";
+    std::cerr << "Wrong parameter values: weapon_dmg_bonus(float, float, float, float)\n";
     return -1;
 }
 
-float artifact_dmg_bonus(int artifact_set_used, float dmgbonus_p, float dmgbonus_pe) {
+float artifact_dmg_bonus(int artifact_set_used, float dmgbonus_all) {
     if (artifact_set_used == blizzard) {
-        return dmgbonus_pe += 15.0F;
+        return dmgbonus_all += 15.0F;
     }
     if (artifact_set_used == wanderers) {
-        return dmgbonus_p += 35;
+        return dmgbonus_all += 35.0F;
     }
-    std::cerr << "Wrong parameter values: artifact_dmg_bonus(int&, float&)\n";
+    std::cerr << "Wrong parameter values: artifact_dmg_bonus(int, float)\n";
     return -1;
-} 
+}
+
